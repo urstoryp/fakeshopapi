00d1df72ad59d8656bab5553759f9c92cde49d42
김성박 <urstory@gmail.com>
2023-02-01 22:45:49 +0900

    jwt token 주석 추가

--- src/test/java/com/example/fakeshopapi/util/JwtTokenizerTest.java
+++ src/test/java/com/example/fakeshopapi/util/JwtTokenizerTest.java
@@ -18,37 +18,43 @@ public class JwtTokenizerTest {
     @Autowired
     JwtTokenizer jwtTokenizer;
 
-    @Value("${jwt.secretKey}") String accessSecret;
-    public final Long ACCESS_TOKEN_EXPIRE_COUNT = 30 * 60 * 1000L;
+    @Value("${jwt.secretKey}") // application.yml파일의 jwt: secretKey: 값
+    String accessSecret; // "12345678901234567890123456789012"
+    public final Long ACCESS_TOKEN_EXPIRE_COUNT = 30 * 60 * 1000L; // 60 * 1000 * 30 // 30분
 
     @Test
-    public void createToken() throws Exception{
+    public void createToken() throws Exception{ // JWT 토큰을 생성.
         String email = "urstory@gmail.com";
-        List<String> roles = List.of("ROLE_USER");
+        List<String> roles = List.of("ROLE_USER"); // [ "ROLE_USER" ]
         Long id = 1L;
-        Claims claims = Jwts.claims().setSubject(email);
-
+        Claims claims = Jwts.claims().setSubject(email); // JWT 토큰의 payload에 들어갈 내용(claims)을 설정.
+        // claims -- sub -- email
+        //        -- roles -- [ "ROLE_USER" ]
+        //        -- userId -- 1L
         claims.put("roles", roles);
         claims.put("userId", id);
 
+        // application.yml파일의 jwt: secretKey: 값
         byte[] accessSecret = this.accessSecret.getBytes(StandardCharsets.UTF_8);
 
-        String JwtToken = Jwts.builder()
-                .setClaims(claims)
+        // JWT를 생성하는 부분.
+        String JwtToken = Jwts.builder() // builder는 JwtBuilder를 반환. Builder패턴.
+                .setClaims(claims) // claims가 추가된 JwtBuilder를 리턴.
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime() + this.ACCESS_TOKEN_EXPIRE_COUNT))
-                .signWith(Keys.hmacShaKeyFor(accessSecret))
+                .setExpiration(new Date(new Date().getTime() + this.ACCESS_TOKEN_EXPIRE_COUNT)) // 현재시간으로부터 30분뒤에 만료.
+                .signWith(Keys.hmacShaKeyFor(accessSecret)) // 결과에 서명까지 포함시킨 JwtBuilder리턴.
                 .compact();
 
         System.out.println(JwtToken);
     }
+    // eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cnN0b3J5QGdtYWlsLmNvbSIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJ1c2VySWQiOjEsImlhdCI6MTY3NTI1ODUxNiwiZXhwIjoxNjc1MjYwMzE2fQ.GPPOAo0KbH06UCI6F6EUqCPpF7ZiqxzHisXOZBhnDWA
 
     @Test
     public void parseToken() throws Exception{
         byte[] accessSecret = this.accessSecret.getBytes(StandardCharsets.UTF_8);
-        String jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cnN0b3J5QGdtYWlsLmNvbSIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJ1c2VySWQiOjEsImlhdCI6MTY3NTI0NDE4NywiZXhwIjoxNjc1MjQ1OTg3fQ.aXyyJNbTHfjAeSow8Go2Li_C_1c6fl7zjwrSSLQQnVA";
+        String jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cnN0b3J5QGdtYWlsLmNvbSIsInJvbGVzIjpbIlJPTEVfVVNFUiIsIkFETUlOX1VTRVIiXSwidXNlcklkIjoxLCJpYXQiOjE2NzUyNTg1MTYsImV4cCI6MTY3NTI2MDMxNn0.GPPOAo0KbH06UCI6F6EUqCPpF7ZiqxzHisXOZBhnDWA";
 
-        Claims claims = Jwts.parserBuilder()
+        Claims claims = Jwts.parserBuilder() // JwtParserBuilder를 반환.
                 .setSigningKey(Keys.hmacShaKeyFor(accessSecret))
                 .build()
                 .parseClaimsJws(jwtToken)
@@ -56,6 +62,8 @@ public class JwtTokenizerTest {
         System.out.println(claims.getSubject());
         System.out.println(claims.get("roles"));
         System.out.println(claims.get("userId"));
+        System.out.println(claims.getIssuedAt());
+        System.out.println(claims.getExpiration());
     }
 
     @Test
